c925fd2947c2e9942e8f0cecb57ff4de461c1e33
himanshusetia08hs <himanshusetia17@gmail.com>
2021-08-07 00:57:36 +0530

    second commit

---
+++ build/precache-manifest.9b1b8c06e70f7888fa0caefcecfb4a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abb0278e359383d5c2eea7955a773eb",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "ad75fbe48c0dfcd543ef",
+    "url": "/react-covid-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "65a7a257c6ce6cf8fdff",
+    "url": "/react-covid-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad75fbe48c0dfcd543ef",
+    "url": "/react-covid-tracker/static/js/2.7af295fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-covid-tracker/static/js/2.7af295fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a7a257c6ce6cf8fdff",
+    "url": "/react-covid-tracker/static/js/main.2a3cf800.chunk.js"
+  },
+  {
+    "revision": "617a620088f6d51556b4",
+    "url": "/react-covid-tracker/static/js/runtime-main.63d8a9cd.js"
+  }
+]);
